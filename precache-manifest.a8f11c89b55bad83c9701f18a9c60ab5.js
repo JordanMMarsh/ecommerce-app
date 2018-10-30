44727b8437aa9fc4ffbbaf9b3c23852c35a91f75
Jordan Marsh <jordanmarshdesign@gmail.com>
2018-10-30 16:53:27 -0500

    Updates

---
+++ precache-manifest.a8f11c89b55bad83c9701f18a9c60ab5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7c3fd3f7cc89a6986458",
+    "url": "/ecommerce-app/static/js/runtime~main.7c3fd3f7.js"
+  },
+  {
+    "revision": "5362a977ec15e66eaa44",
+    "url": "/ecommerce-app/static/js/main.5362a977.chunk.js"
+  },
+  {
+    "revision": "a93cec8a557ee382682a",
+    "url": "/ecommerce-app/static/js/1.a93cec8a.chunk.js"
+  },
+  {
+    "revision": "5362a977ec15e66eaa44",
+    "url": "/ecommerce-app/static/css/main.6ffa7278.chunk.css"
+  },
+  {
+    "revision": "4765847b5a98115e477229a6bc44e4e6",
+    "url": "/ecommerce-app/index.html"
+  }
+];
